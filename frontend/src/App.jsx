f978e1e0a9a311d38fdb3eb382b9e0109fec30b6
TUSHAR RAJPUT <114666939+tusharrajput70@users.noreply.github.com>
2024-10-12 16:36:08 +0530

    error correction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import './index.css'
 import {Toaster} from 'react-hot-toast';
-import Home from './pages/home/home'
+import Home from './pages/home/Home'
 import Login from './pages/login/login'
 import SignUp from './pages/signup/SignUp'
 import { useAuthContext } from './context/AuthContext';
