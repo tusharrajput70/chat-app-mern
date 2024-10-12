4c33ad7b5331d9236d62cbc4a1d061f5f2fb7c80
TUSHAR RAJPUT <114666939+tusharrajput70@users.noreply.github.com>
2024-10-12 16:44:02 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider=({children})=>{
 
     useEffect(()=>{
         if(authUser){
-            const socket=io("http://localhost:5000",{
+            const socket=io("https://chat-app-6pms.onrender.com",{
                 query:{
                     userId:authUser._id,
                 },
